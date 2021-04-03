1ddaacec9cc073e016f3619828a6fd3eaabbb1ab
shubham5i2 <shubhamkumarsingh450@gmail.com>
2021-04-04 00:53:21 +0530

    commit

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,8 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("https://sks-video-chat-app.herokuapp.com/");
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
